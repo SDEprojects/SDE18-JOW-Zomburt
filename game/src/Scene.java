32cbc594c1c640df35e39b83ab8c7a88d1773f23
itsOrD <ord.sc2@gmail.com>
2020-08-28 11:48:48 -0700

    rough draft of scene generator without API logic nor 'generating' working

---
+++ game/src/Scene.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Scene {
+    ArrayList<String> items = new ArrayList<String>();
+    ArrayList<String> enemies = new ArrayList<String>();
+    String flavorText = "";
+
+    public Scene(String name) {
+        // generate instance of scene
+    }
+
+    public ArrayList<String> getItems() {
+        return items;
+    }
+    private void setItems(ArrayList<String> itemsToAdd, boolean overwrite) {
+        if (overwrite) {
+            items = itemsToAdd;
+        } else {
+            for (String i : itemsToAdd) {
+                items.add(i);
+            }
+        }
+    }
+
+    public String getFlaovrText() {
+        return flavorText;
+    }
+    private void setFlavorText(String name) {
+        // access API
+    }
+}
