96291510927e0afd5f7caedb1bf07a883b881203
itsOrD <ord.sc2@gmail.com>
2020-09-01 08:57:22 -0700

    working on YAML parsing with Java

--- game/src/com/zomburt/SceneFactory.java
+++ game/src/com/zomburt/SceneFactory.java
@@ -2,16 +2,20 @@ package com.zomburt;
 
 import java.util.ArrayList;
 
+import zapi from "../../assets/api.yaml";
+
 public class SceneFactory {
 
     static Scene createScene(String name) {
         return new Scene(name);
     }
 
-//    public static ArrayList<Scene> createUniverse(String name) {
-//        // reach into api for scene data based on name
-//    }
+    public static ArrayList<Scene> createUniverse(String name) {
+        // reach into api for scene data based on name
+        System.out.println(zapi);
+    }
 
+    // @Override (kind of) of toString
     public static StringBuilder universeMap(ArrayList<Scene> currentGame) {
         StringBuilder result = new StringBuilder(
                 "So far your universe contains the following scenes: \n ");
