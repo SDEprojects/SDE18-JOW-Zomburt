c6cdb87aa3de28b293e7866d8db58fa554493719
gtulkn12 <45664554+gtulkn12@users.noreply.github.com>
2020-09-08 12:21:30 -0700

    pulled Master

---
+++ game/src/com/zomburt/combat/Weapons.java
@@ -0,0 +1,11 @@
+package com.zomburt.combat;
+
+public class Weapons {
+  private String name;
+  private int damage;
+
+  public Weapons(String name, int damage) {
+    this.name = name;
+    this.damage = damage;
+  }
+}
